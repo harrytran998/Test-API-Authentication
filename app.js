3d81dd83738d3767e483b47ef1cd36595963eddf
Loser Teach <jetaimefrc@gmail.com>
2018-09-08 11:26:30 +0700

    Add module Body-parser

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/order');
+
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+    let err = new Error('Not found !');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        err: {
+            message: err.message
+        }
+    });
+});
 module.exports = app;
