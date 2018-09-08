5ebbb34353266f139c1fb872b23ceba657585470
Loser Teach <jetaimefrc@gmail.com>
2018-09-08 21:46:25 +0700

    Update router get and post/postID

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -8,21 +10,52 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-    let product = {
+
+    let product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(200).json({
-        message: 'Handling POST request to/products',
-        createdProduct: product
     });
+
+    product.save().then(result => {
+        console.log(result);
+        res.status(200).json({
+            message: 'Oh POST ne !',
+            createdProduct: result
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+
 });
 
 router.get('/:productID', (req, res, next) => {
     let id = req.params.productID;
-    res.status(200).json({
-        message: `You get information of productID ${id}`
-    });
+
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            console.log(doc);
+            if (doc) {
+                res.status(200).json(doc);
+            } else {
+                res
+                    .status(404)
+                    .json({
+                        message: "No valid entry found for provided ID"
+                    });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
 });
 
 router.patch('/:productID', (req, res, next) => {
--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/order');
+
+mongoose.connect('mongodb+srv://admin:' +
+    process.env.MONGO_ATLAS_PW +
+    '@cluster0-cb0ti.mongodb.net/test?retryWrites=true');
+
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
@@ -17,8 +23,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
         res.header('Access-Cotrol-Allow-Methods', 'PUT, POST, PATCH, DELETE');
-        return res.status(200).json({
-        });
+        return res.status(200).json({});
     }
     next();
 });
